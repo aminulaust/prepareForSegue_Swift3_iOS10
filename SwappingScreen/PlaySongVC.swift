397796f6c0c8d111d853245e9bd8c4905dd364d1
Aminul <aminulaust@gmail.com>
2017-08-14 23:42:03 +0600

    first commit

---
+++ SwappingScreen/PlaySongVC.swift
@@ -0,0 +1,35 @@
+//
+//  PlaySongVC.swift
+//  SwappingScreen
+//
+//  Created by Appcelerator on 8/14/17.
+//  Copyright © 2017 Appcelerator. All rights reserved.
+//
+
+import UIKit
+
+class PlaySongVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
